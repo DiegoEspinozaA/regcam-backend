9f98ebc8daf19bff4de5cbd39f1fd6e0a81fa175
DiegoEspinozaA <diego.espinozaa@alumnos.uv.cl>
2023-11-07 23:47:40 -0300

    implementacion del historial para los regitros

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const server = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const session = require('express-session');
+const jwt = require('jsonwebtoken');
 
+server.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -13,6 +24,54 @@ const db = mysql.createPool({
 
 server.use(express.json());
 server.use(cors());
+
+//login
+
+server.get('/', (req, res) => {
+    if (req.session.username) {
+        return res.json({ valid: true, role: req.session.role });
+    } else {
+        return res.json({ valid: false });
+    }
+})
+server.post('/sigup', (req, res) => {
+    const sql = "insert into users (username, password) values (?)";
+    const values = [
+        req.body.username,
+        req.body.password
+    ]
+
+    db.query(sql, [values], (err, result) => {
+        if (err) return res.json({ Message: "Error en el servidor" });
+        return res.json(result);
+    })
+})
+
+server.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Realizar una consulta a la base de datos para verificar las credenciales del usuario
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+
+    db.query(sql, [username, password], (err, results) => {
+        if (err) {
+            console.error('Error al consultar la base de datos:', err);
+            res.status(500).json({ error: 'Error en el servidor' });
+        } else if (results.length > 0) {
+            const user = results[0];
+
+            // Generar un token JWT con el rol del usuario
+            const token = jwt.sign({ username: user.username, role: user.role }, 'tu_secreto_secreto', {
+                expiresIn: '1h', // Tiempo de expiración del token
+            });
+
+            res.json({ token, user });
+        } else {
+            // Credenciales incorrectas
+            res.status(401).json({ error: 'Credenciales incorrectas' });
+        }
+    });
+});
 
 server.post("/registrarEvento", (req, res) => {
     const { fecha } = req.body;
@@ -32,7 +91,7 @@ server.post("/registrarEvento", (req, res) => {
 
 server.get("/registrosCamara/:id", (req, res) => {
     const { id } = req.params;
-    let sql = "select registros.id, registros.tipo, registros.fecha, registros.descripcion from registros join camara on registros.id_camara = camara.id where camara.id = ?";
+    let sql = "select registros.id, registros.tipo, registros.fecha, eventos.color, registros.descripcion from registros join camara on registros.id_camara join eventos on eventos.tipo = registros.tipo = camara.id where camara.id = ?";
     db.query(sql, [id], (err, result) => {
         if (err) {
             console.log(err);
@@ -90,29 +149,29 @@ server.get("/locaciones", (req, res) => {
 server.post("/registrarEstados", (req, res) => {
     const datos = req.body;
     const insertPromises = datos.map((item) => {
-      const { id, evento, fecha } = item;
-      const sql = "INSERT INTO Estados (id_camara, evento, fecha) VALUES (?,?,?)";
-      const values = [id, evento, fecha];
-      return new Promise((resolve, reject) => {
-        db.query(sql, values, (err, result) => {
-          if (err) {
-            console.error(err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
+        const { id, evento, fecha } = item;
+        const sql = "INSERT INTO Estados (id_camara, evento, fecha) VALUES (?,?,?)";
+        const values = [id, evento, fecha];
+        return new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
         });
-      });
     });
-  
+
     Promise.all(insertPromises)
-      .then((results) => {
-        res.json(results);
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Error interno del servidor' });
-      });
-  });
+        .then((results) => {
+            res.json(results);
+        })
+        .catch((error) => {
+            res.status(500).json({ error: 'Error interno del servidor' });
+        });
+});
 
 
 server.get("/camaras", (req, res) => {
@@ -152,15 +211,27 @@ server.post("/registrarEstado", (req, res) => {
     })
 });
 
+server.get("/historial/:id", (req, res) => {
+    const id = req.params.id;
+
+    let sql = "SELECT * from historial where id_registro = ?";
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: "Hubo un error en la consulta a la base de datos" });
+        } else {
+            res.send(result);
+        }
+    })
+})
 server.post("/guardarRegistro", (req, res) => {
-    const { id } = req.body;
+    const { id_camara } = req.body;
     const { tipo } = req.body;
     const { fecha } = req.body;
     const { descripcion } = req.body
     let sql = "insert into registros (tipo, fecha, descripcion, id_camara) values (?,?,?,?)";
-    db.query(sql, [tipo, fecha, descripcion, id], (err, result) => {
+    db.query(sql, [tipo, fecha, descripcion, id_camara], (err, result) => {
         if (err) {
-            console.log(err);
+            res.status(500).json({ error: "Hubo un error en la consulta a la base de datos" });
         } else {
             res.send(result);
         }
@@ -171,9 +242,9 @@ server.post("/guardarRegistro", (req, res) => {
 server.put("/actualizarRegistro/:id", (req, res) => {
     const registroId = req.params.id;
     const { tipo, fecha, descripcion } = req.body;
-    
+
     let sql = "UPDATE registros SET tipo = ?, fecha = ?, descripcion = ? WHERE id = ?";
-    
+
     db.query(sql, [tipo, fecha, descripcion, registroId], (err, result) => {
         if (err) {
             console.log(err);
